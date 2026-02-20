b3218f008fa5648ca600342188090218f81ac8c0
amr0mengd <amrmengd0@protonmail.com>
2026-02-20 06:30:36 +0200

    added a function to change language (not doing anything for now)

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="text-[8px] flex items-center h-6">
-    <button class="px-1 py-1.5 border border-primary bg-primary rounded-l-sm text-white underline font-bold">EN</button>
-    <button class="px-1 py-1.5 border border-primary rounded-r-sm">JP</button>
+    <button
+      @click="setLang('en')"
+      class="px-1 py-1.5 border border-primary bg-primary rounded-l-sm text-white underline font-bold"
+    >
+      EN
+    </button>
+    <button @click="setLang('jp')" class="px-1 py-1.5 border border-primary rounded-r-sm">
+      JP
+    </button>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      activeLang: 'en',
+    }
+  },
+  methods: {
+    setLang(lang = 'en') {
+      this.activeLang = lang;
+    },
+  },
+}
+</script>
